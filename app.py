cc555d0db6f3253db911ed2bb33c5804e24fb016
Lucius Fox <lucius-fox-gg@protonmail.com>
2024-02-22 14:03:18 +0100

    Updating app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import antigravity
 
-CERT="""-----BEGIN EC PRIVATE KEY-----
-MHcCAQEEIBEpNV5hXNiilrD2BavXRukPp7HG8t9ISsX4dygHvglyoAoGCCqGSM49
-AwEHoUQDQgAEKfeT3y+t9Ndah87r2kpwOlUnfHgS+WtwZmet8ZUPoJhQaFWZe9UK
-6PywajWHsLOSQZL2RiYn0Hr2zqrmNB26fg==
------END EC PRIVATE KEY-----
-"""
+SEED=3af9c989-4eb6-4272-b05c-8d9aaf1ada8b
 
 def main():
     antigravity.fly()
